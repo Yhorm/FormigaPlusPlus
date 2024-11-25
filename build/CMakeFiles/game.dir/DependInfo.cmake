f4e9b1a0c1b74be0ad5f91e48b66bf68313ca709
andrezin <andrcosta72@gmail.com>
2024-11-24 21:57:58 -0300

    adicionando o meu makefile pfvr nao deleta

---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/andre/tec-prog-proj-final/src/Ente.cpp" "CMakeFiles/game.dir/src/Ente.cpp.o" "gcc" "CMakeFiles/game.dir/src/Ente.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/Entidade.cpp" "CMakeFiles/game.dir/src/Entidade.cpp.o" "gcc" "CMakeFiles/game.dir/src/Entidade.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/GerenciadorColisoes.cpp" "CMakeFiles/game.dir/src/GerenciadorColisoes.cpp.o" "gcc" "CMakeFiles/game.dir/src/GerenciadorColisoes.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/GerenciadorEventos.cpp" "CMakeFiles/game.dir/src/GerenciadorEventos.cpp.o" "gcc" "CMakeFiles/game.dir/src/GerenciadorEventos.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/GerenciadorGrafico.cpp" "CMakeFiles/game.dir/src/GerenciadorGrafico.cpp.o" "gcc" "CMakeFiles/game.dir/src/GerenciadorGrafico.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/Inimigo.cpp" "CMakeFiles/game.dir/src/Inimigo.cpp.o" "gcc" "CMakeFiles/game.dir/src/Inimigo.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/Jogador.cpp" "CMakeFiles/game.dir/src/Jogador.cpp.o" "gcc" "CMakeFiles/game.dir/src/Jogador.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/Jogo.cpp" "CMakeFiles/game.dir/src/Jogo.cpp.o" "gcc" "CMakeFiles/game.dir/src/Jogo.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/Obstaculo.cpp" "CMakeFiles/game.dir/src/Obstaculo.cpp.o" "gcc" "CMakeFiles/game.dir/src/Obstaculo.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/Personagem.cpp" "CMakeFiles/game.dir/src/Personagem.cpp.o" "gcc" "CMakeFiles/game.dir/src/Personagem.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/Plataforma.cpp" "CMakeFiles/game.dir/src/Plataforma.cpp.o" "gcc" "CMakeFiles/game.dir/src/Plataforma.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/Projetil.cpp" "CMakeFiles/game.dir/src/Projetil.cpp.o" "gcc" "CMakeFiles/game.dir/src/Projetil.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/listEntidade.cpp" "CMakeFiles/game.dir/src/listEntidade.cpp.o" "gcc" "CMakeFiles/game.dir/src/listEntidade.cpp.o.d"
+  "/home/andre/tec-prog-proj-final/src/main.cpp" "CMakeFiles/game.dir/src/main.cpp.o" "gcc" "CMakeFiles/game.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/src/Ente.cpp.o"
+  "CMakeFiles/game.dir/src/Ente.cpp.o.d"
+  "CMakeFiles/game.dir/src/Entidade.cpp.o"
+  "CMakeFiles/game.dir/src/Entidade.cpp.o.d"
+  "CMakeFiles/game.dir/src/GerenciadorColisoes.cpp.o"
+  "CMakeFiles/game.dir/src/GerenciadorColisoes.cpp.o.d"
+  "CMakeFiles/game.dir/src/GerenciadorEventos.cpp.o"
+  "CMakeFiles/game.dir/src/GerenciadorEventos.cpp.o.d"
+  "CMakeFiles/game.dir/src/GerenciadorGrafico.cpp.o"
+  "CMakeFiles/game.dir/src/GerenciadorGrafico.cpp.o.d"
+  "CMakeFiles/game.dir/src/Inimigo.cpp.o"
+  "CMakeFiles/game.dir/src/Inimigo.cpp.o.d"
+  "CMakeFiles/game.dir/src/Jogador.cpp.o"
+  "CMakeFiles/game.dir/src/Jogador.cpp.o.d"
+  "CMakeFiles/game.dir/src/Jogo.cpp.o"
+  "CMakeFiles/game.dir/src/Jogo.cpp.o.d"
+  "CMakeFiles/game.dir/src/Obstaculo.cpp.o"
+  "CMakeFiles/game.dir/src/Obstaculo.cpp.o.d"
+  "CMakeFiles/game.dir/src/Personagem.cpp.o"
+  "CMakeFiles/game.dir/src/Personagem.cpp.o.d"
+  "CMakeFiles/game.dir/src/Plataforma.cpp.o"
+  "CMakeFiles/game.dir/src/Plataforma.cpp.o.d"
+  "CMakeFiles/game.dir/src/Projetil.cpp.o"
+  "CMakeFiles/game.dir/src/Projetil.cpp.o.d"
+  "CMakeFiles/game.dir/src/listEntidade.cpp.o"
+  "CMakeFiles/game.dir/src/listEntidade.cpp.o.d"
+  "CMakeFiles/game.dir/src/main.cpp.o"
+  "CMakeFiles/game.dir/src/main.cpp.o.d"
+  "game"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
