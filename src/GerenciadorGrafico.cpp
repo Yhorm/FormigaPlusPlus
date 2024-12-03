87be04de132861ecb8ea116e1694ce05acbbc049
andre <andrcosta72@gmail.com>
2024-12-03 09:31:07 -0300

    camera

--- src/GerenciadorGrafico.cpp
+++ src/GerenciadorGrafico.cpp
@@ -3,7 +3,8 @@
 GerenciadorGrafico* Gerenciadores::GerenciadorGrafico::pGerGraf(nullptr);
 
 Gerenciadores::GerenciadorGrafico::GerenciadorGrafico() :
-    window(new RenderWindow(VideoMode(1280, 720), "Jogasso", Style::Default))
+    window(new RenderWindow(VideoMode(1280, 720), "Jogasso", Style::Default)),
+	cam(sf::Vector2f(window->getSize().x/2,window->getSize().y/2),sf::Vector2f(window->getSize()))
 {
     if (window == nullptr)
     {
@@ -12,6 +13,7 @@ Gerenciadores::GerenciadorGrafico::GerenciadorGrafico() :
     }
     else
     {
+		window->setView(cam);
         window->setFramerateLimit(60);
     }
 }
@@ -30,6 +32,11 @@ Gerenciadores::GerenciadorGrafico::GerenciadorGrafico() :
          pGerGraf = new GerenciadorGrafico;
      return pGerGraf;
 }
-
-
-
+void Gerenciadores::GerenciadorGrafico::centralize(const Vector2f pos){
+	cam.setCenter(sf::Vector2f(pos.x,pos.y));
+	window->setView(cam);
+}
+void Gerenciadores::GerenciadorGrafico::size(const Vector2f tam){
+		cam.setSize(sf::Vector2f(tam.x,tam.y));
+		window->setView(cam);
+}
