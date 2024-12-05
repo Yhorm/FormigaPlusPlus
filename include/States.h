9940ea20ce4b879398abf97a5312159de89b73a0
Felipe da Silva Mossato <demongo90@hotmail.com>
2024-12-05 12:05:36 -0300

    Criação dos estados

---
+++ include/States.h
@@ -0,0 +1,30 @@
+#include <iostream>
+
+namespace States
+{
+    enum Game_State
+    {
+        STATE_INGAME,
+        STATE_PAUSED,
+        STATE_MAIN_MENU,
+        STATE_OPTIONS_MENU,
+        STATE_SAVE_MENU,
+        STATE_STAGE_SELECTION
+    };
+
+    enum Player_State
+    {
+        PLAYER_IN_MOVEMENT,
+        PLAYER_IDLE,
+        PLAYER_JUMPING,
+        PLAYER_DOUBLE_JUMPING,
+        PLAYER_DASHING
+    };
+
+    enum Enemy_State
+    {
+        ENEMY_IDLE,
+        ENEMY_CHASING,
+        ENEMY_ATTACKING
+    };
+}
