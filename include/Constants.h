2325927d9b951c5cd2a3559d7b1f6ac2242867c0
andre <andrcosta72@gmail.com>
2024-12-11 11:11:55 -0300

    realizando as alterações sobre determinados tipos de problemas e adicionando fase

--- include/Constants.h
+++ include/Constants.h
@@ -2,18 +2,18 @@
 
 namespace Constants
 {
-	constexpr float GRAVITY = 9.81  ;
+	constexpr float GRAVITY = 8.81  ;
 	constexpr float DELTATIME = 0.8  ;
     constexpr float STRT_VEL_PLYR_X = 2.5;
-	constexpr float VEL_PLAYER_X = 5.0;
-	constexpr float VEL_PLAYER_Y = 4.0;
+	constexpr float VEL_PLAYER_X = 8.0;
+	constexpr float VEL_PLAYER_Y = 7.0;
     constexpr float JMP_HEIGHT = 200.0;
     constexpr float VEL_ENEMY_X = 1.0;
     constexpr float VEL_ENEMY_Y = 1.0;
     constexpr float E_TRGT_RADIUS_X = 200.0;
     constexpr float E_TRGT_RADIUS_Y = 200.0;
     constexpr float SIZE_PLYR_H = 75.0;
-    constexpr float SIZE_PLYR_W = 75.0;
+    constexpr float SIZE_PLYR_W = 100.0;
     constexpr float SIZE_ENEMY_H = 50.0;
     constexpr float SIZE_ENEMY_W = 50.0;
     constexpr float SIZE_PLTFORM_H = 50.0;
